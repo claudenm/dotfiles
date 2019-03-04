770a40fed43e0295bf173aa6214b998d0a7fbd1d
claudenm <claude.mendelson@gmail.com>
2019-03-04 14:54:39 -0500

    additional aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,3 +1,14 @@
 alias reload!='. ~/.zshrc'
 
 alias cls='clear' # Good 'ol Clear Screen command
+
+alias k='kubectl'
+
+alias gso='git stash && git checkout master && git pull'
+
+alias chrome='open -a "Google Chrome"'
+
+haadmin() {
+  for lb in $(bin/list -d $1 -e production -r load_balancer); do chrome http://$lb:9000/haproxy_stats;done; 
+}
+
