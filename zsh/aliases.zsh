9476205fdba232c7426ae0b5e71165f75374abb2
claudenm <claude.mendelson@gmail.com>
2019-04-04 11:00:56 -0400

    peloton stuff

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -8,7 +8,20 @@ alias gso='git stash && git checkout master && git pull'
 
 alias chrome='open -a "Google Chrome"'
 
-haadmin() {
-  for lb in $(bin/list -d $1 -e production -r load_balancer); do chrome http://$lb:9000/haproxy_stats;done; 
-}
+alias python=/usr/local/bin/python3
 
+alias python2=/usr/bin/python 
+
+fixvpn () {
+    echo "Flushing routes..."
+    for i in $(ifconfig | egrep -o "^[a-z].+\d{1}:" | sed 's/://');
+    do
+        sudo ifconfig "$i" down;
+    done;
+    sudo route -n flush;
+    for i in $(ifconfig | egrep -o "^[a-z].+\d{1}:" | sed 's/://');
+    do
+        sudo ifconfig "$i" up;
+    done
+    echo "All done"
+}
